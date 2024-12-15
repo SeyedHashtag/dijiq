b0066b51086728ca3c19d078afba1ff239458807
Seyed Mahdi <39972836+SeyedHashtag@users.noreply.github.com>
2024-12-16 02:11:36 +0330

    Update command.py

--- core/scripts/telegrambot/utils/command.py
+++ core/scripts/telegrambot/utils/command.py
@@ -9,18 +9,10 @@ from telebot import types
 load_dotenv()
 
 API_TOKEN = os.getenv('API_TOKEN')
-if not API_TOKEN:
-    raise ValueError("No API_TOKEN found in environment variables")
-
-ADMIN_USER_IDS = json.loads(os.getenv('ADMIN_USER_IDS', '[]'))
+ADMIN_USER_IDS = json.loads(os.getenv('ADMIN_USER_IDS'))
 CLI_PATH = '/etc/hysteria/core/cli.py'
 BACKUP_DIRECTORY = '/opt/hysbackup'
-
-print(f"Initializing bot with token: {API_TOKEN[:6]}...{API_TOKEN[-4:]}")
 bot = telebot.TeleBot(API_TOKEN)
-
-MERCHANT_ID = os.getenv('CRYPTOMUS_MERCHANT_ID')
-PAYMENT_API_KEY = os.getenv('CRYPTOMUS_API_KEY')
 
 def run_cli_command(command):
     try:
@@ -28,7 +20,7 @@ def run_cli_command(command):
         result = subprocess.check_output(args, stderr=subprocess.STDOUT)
         return result.decode('utf-8').strip()
     except subprocess.CalledProcessError as e:
-        return get_text("en", "cli_error").format(error=e.output.decode("utf-8"))
+        return f'Error: {e.output.decode("utf-8")}'
 
 def is_admin(user_id):
     return user_id in ADMIN_USER_IDS
