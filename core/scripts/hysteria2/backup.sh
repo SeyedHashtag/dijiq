28c4e6a9636243a21f0a49643d401b7e7e6def7f
Seyed Mahdi <39972836+SeyedHashtag@users.noreply.github.com>
2024-12-17 02:25:24 +0330

    Update backup.sh

--- core/scripts/hysteria2/backup.sh
+++ core/scripts/hysteria2/backup.sh
@@ -17,7 +17,7 @@ FILES_TO_BACKUP=(
     "/etc/hysteria/core/scripts/telegrambot/payments.json"
     "/etc/hysteria/core/scripts/telegrambot/plans.json"
     "/etc/hysteria/test_mode.json"
-    "/etc/hysteria/statistics.json"
+    "/etc/hysteria/core/scripts/telegrambot/support_info.json"
 )
 
 zip -j "$BACKUP_FILE" "${FILES_TO_BACKUP[@]}" >/dev/null
