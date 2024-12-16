c3585498bc6136701f57b6903670a4c26d5c9719
Seyed Mahdi <39972836+SeyedHashtag@users.noreply.github.com>
2024-12-17 02:18:43 +0330

    Update tbot.py

--- core/scripts/telegrambot/tbot.py
+++ core/scripts/telegrambot/tbot.py
@@ -13,22 +13,25 @@ from utils.admin_plans import *
 from utils.admin_test_mode import *
 from utils.admin_support import *
 from utils.admin_broadcast import *
-from utils.admin_stats import *
-from utils.client_handlers import ClientHandlers
-
-# Initialize client handlers
-client_handlers = ClientHandlers(bot)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    if is_admin(message.from_user.id):
-        markup = create_main_markup(is_admin=True)
+    is_admin_user = is_admin(message.from_user.id)
+    markup = create_main_markup(is_admin=is_admin_user)
+    
+    if is_admin_user:
         bot.reply_to(message, "Welcome to the Admin Panel!", reply_markup=markup)
     else:
-        client_handlers.handle_client_start(message)
-
-# Register client handlers
-client_handlers.register_handlers()
+        welcome_text = (
+            "Welcome to our VPN Service! 🌐\n\n"
+            "Here you can:\n"
+            "📱 View your configs\n"
+            "💰 Purchase new plans\n"
+            "⬇️ Download our apps\n"
+            "📞 Get support\n\n"
+            "Please use the menu below to get started!"
+        )
+        bot.reply_to(message, welcome_text, reply_markup=markup)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
