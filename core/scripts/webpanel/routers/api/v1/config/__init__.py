68dde4f8638837d116cfbde6df4524441273a908
Whispering Wind <151555003+ReturnFI@users.noreply.github.com>
2025-08-17 15:39:20 +0330

    feat: add webpanel API for extra subscription configs

--- core/scripts/webpanel/routers/api/v1/config/__init__.py
+++ core/scripts/webpanel/routers/api/v1/config/__init__.py
@@ -6,6 +6,7 @@ from . import normalsub
 from . import singbox
 from . import ip
 from . import misc
+from . import extra_config
 
 router = APIRouter()
 
@@ -16,4 +17,5 @@ router.include_router(telegram.router, prefix='/telegram')
 router.include_router(normalsub.router, prefix='/normalsub')
 router.include_router(singbox.router, prefix='/singbox')
 router.include_router(ip.router, prefix='/ip')
+router.include_router(extra_config.router, prefix='/extra-config', tags=['Config - Extra Config'])
 router.include_router(misc.router)
--- core/scripts/webpanel/routers/api/v1/schema/config/__init__.py
+++ core/scripts/webpanel/routers/api/v1/schema/config/__init__.py
@@ -3,3 +3,4 @@ from . import normalsub
 from . import singbox
 from . import telegram
 from . import warp
+from . import extra_config
