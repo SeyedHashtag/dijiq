ba72f0e9d8e40f8f73dba368b1b01507caf545a9
ReturnFI <151555003+ReturnFI@users.noreply.github.com>
2025-12-31 20:26:35 +0000

    refactor: introduce settings submenu in Telegram bot
    Co-authored-by: ReturnFI <151555003+ReturnFI@users.noreply.github.com>

--- core/scripts/telegrambot/utils/__init__.py
+++ core/scripts/telegrambot/utils/__init__.py
@@ -9,3 +9,4 @@ from .serverinfo import *
 from .cpu import *
 from .check_version import *
 from .weburl import *
+from .settings import *
--- core/scripts/telegrambot/utils/common.py
+++ core/scripts/telegrambot/utils/common.py
@@ -4,5 +4,11 @@ def create_main_markup():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.row('➕ Add User', '🔍 Show User')
     markup.row('🗑️ Delete User', '🖥️ Server Info')
-    markup.row('💾 Backup Server', '🔗 Get Webpanel URL')
+    markup.row('💾 Backup Server', '⚙️ Settings')
+    return markup
+
+def create_settings_markup():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row('🔗 Get Webpanel URL')
+    markup.row('⬅️ Back')
     return markup
