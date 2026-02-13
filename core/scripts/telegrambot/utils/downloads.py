bfe002412dba377bc0c134ea547b44d093e92bd8
Seyed Mahdi <39972836+SeyedHashtag@users.noreply.github.com>
2026-02-13 21:07:24 +0330

    chore(telegrambot): update download links to latest versions

--- core/scripts/telegrambot/utils/downloads.py
+++ core/scripts/telegrambot/utils/downloads.py
@@ -8,14 +8,15 @@ from utils.language import get_user_language
 DOWNLOAD_LINKS = {
     "karing": {
         "ios": "https://apps.apple.com/ca/app/karing/id6472431552",
-        "android": "https://github.com/KaringX/karing/releases/download/v1.2.10.1308/karing_1.2.10.1308_android_arm64-v8a.apk",
-        "windows": "https://github.com/KaringX/karing/releases/download/v1.2.10.1308/karing_1.2.10.1308_windows_x64.exe"
+        "android": "https://github.com/KaringX/karing/releases/download/v1.2.14.1701/karing_1.2.14.1701_android_arm64-v8a.apk",
+        "windows": "https://github.com/KaringX/karing/releases/download/v1.2.14.1701/karing_1.2.14.1701_windows_x64.exe"
     },
     "singbox": {
-        "android": "https://github.com/SagerNet/sing-box/releases/download/v1.12.14/SFA-1.12.14-arm64-v8a.apk"
+        "android": "https://github.com/SagerNet/sing-box/releases/download/v1.13.0-rc.3/SFA-1.13.0-rc.3-arm64-v8a.apk"
     },
     "v2ray": {
-        "windows": "https://github.com/2dust/v2rayN/releases/download/7.16.9/v2rayN-windows-64-SelfContained.zip"
+        "windows": "https://github.com/2dust/v2rayN/releases/download/7.17.3/v2rayN-windows-64-SelfContained.zip",
+        "android": "https://github.com/2dust/v2rayNG/releases/download/2.0.9/v2rayNG_2.0.9_arm64-v8a.apk"
     }
 }
 
