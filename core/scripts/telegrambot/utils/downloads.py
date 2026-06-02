d40e135fad389a5e2eb402d319a08cb616f11c24
Seyed Mahdi <39972836+SeyedHashtag@users.noreply.github.com>
2026-06-02 15:12:16 +0330

    fix(downloads): Update Karing Windows download link and add Happ iOS link

--- core/scripts/telegrambot/utils/downloads.py
+++ core/scripts/telegrambot/utils/downloads.py
@@ -8,11 +8,14 @@ from utils.language import get_user_language
 DOWNLOAD_LINKS = {
     "karing": {
         "ios": "https://apps.apple.com/ca/app/karing/id6472431552",
-        "windows": "https://github.com/KaringX/karing/releases/download/v1.2.18.2102/karing_1.2.18.2102_windows_x64.exe"
+        "windows": "https://github.com/KaringX/karing/releases/download/v1.2.19.2209/karing_1.2.19.2209_windows_x64.exe"
     },
     "v2ray": {
         "windows": "https://github.com/2dust/v2rayN/releases/download/7.22.3/v2rayN-windows-64.zip",
         "android": "https://github.com/2dust/v2rayNG/releases/download/2.2.0/v2rayNG_2.2.0_arm64-v8a.apk"
+    },
+    "happ": {
+        "ios": "https://apps.apple.com/us/app/happ-proxy-utility/id6504287215"
     }
 }
 
